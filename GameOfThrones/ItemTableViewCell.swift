14cf4d5950e96625543ad6bd44259f42492fd72e
Kelderth <Kelderth@gmail.com>
2019-03-25 10:57:40 -0400

    First 2 hours of work - achievement.

---
+++ GameOfThrones/ItemTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemTableViewCell.swift
+//  GameOfThrones
+//
+//  Created by Eduardo Santiz on 3/24/19.
+//  Copyright © 2019 EduardoSantiz. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+    @IBOutlet weak var episodeNumberLabel: UILabel!
+    @IBOutlet weak var episodeTitleLabel: UILabel!
+    
+}
--- GameOfThrones/TableViewController.swift
+++ GameOfThrones/TableViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class TableViewController: UIViewController {
 
+    // MARK: - Properties
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
 
 }
 
+extension TableViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Season"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? ItemTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "DetailViewController")
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
